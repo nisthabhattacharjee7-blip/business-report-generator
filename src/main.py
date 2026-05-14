199a61a1c0fe0c1f08a09cba70f9d502aec17472
Nistha <nisthabhattacharjee7@gmail.com>
2026-05-14 11:50:24 +0530

    done final editing in main

--- src/main.py
+++ src/main.py
@@ -1,27 +1,59 @@
-import pandas as pd 
+import pandas as pd
+import json
+
 from cleaner import clean_data
-from analyzer import analyze_df 
+from analyzer import analyze_df
 from reporter import generate_report
+from utils import log_message
 
-df = pd.read_csv("data/sales.csv")
 
-# original data 
-print("\n=== ORIGINAL DATA ===")
-print(df)
+try:
 
-# clean data 
-cleaned_df = clean_data(df)
+    # Start pipeline
+    log_message("Pipeline started")
 
-print("\n=== CLEANED DATA ===")
-print(cleaned_df)
+    # Load CSV
+    df = pd.read_csv("data/sales.csv")
+    log_message("CSV loaded successfully")
 
-analysis = analyze_df(cleaned_df)
-print("\n=== ANALYSIS REPORTS ===")
+    print("\n=== ORIGINAL DATA ===")
+    print(df)
 
-generate_report(cleaned_df, analysis)
+    # Clean data
+    cleaned_df = clean_data(df)
+    log_message("Data cleaned successfully")
 
-total_revenue, avg_revenue, top_products = analysis
-print("\nTotal Revenue:", total_revenue)
-print("Average Revenue:", avg_revenue)
-print("Top Product:", top_products)
+    print("\n=== CLEANED DATA ===")
+    print(cleaned_df)
 
+    # Analyze data
+    analysis = analyze_df(cleaned_df)
+    log_message("Data analyzed successfully")
+
+    print("\n=== ANALYSIS REPORTS ===")
+
+    # Generate reports
+    generate_report(cleaned_df, analysis)
+    log_message("Reports generated successfully")
+
+    # Load config file
+    with open("config.json", "r") as config_file:
+        config = json.load(config_file)
+
+    print("\nCompany Name:", config["company_name"])
+
+    # Unpack analysis results
+    total_revenue, avg_revenue, top_products = analysis
+
+    print("\nTotal Revenue:", total_revenue)
+    print("Average Revenue:", avg_revenue)
+    print("Top Product:", top_products)
+
+    log_message("Pipeline completed successfully")
+
+
+except Exception as e:
+
+    print("Error:", e)
+
+    log_message(f"Error occurred: {e}")
