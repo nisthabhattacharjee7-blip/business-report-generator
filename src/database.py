e434f2ee979d48ac11ec4bb7e2878cc367786223
Nistha <nisthabhattacharjee7@gmail.com>
2026-05-15 16:24:27 +0530

    added data

--- src/database.py
+++ src/database.py
@@ -8,7 +8,25 @@ def create_table(con):
     cursor = con.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXISTS sales
                       (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                       product_name TEXT NOT NULL,
+                       product TEXT NOT NULL,
                        quantity INTEGER NOT NULL,
                        price REAL NOT NULL)''')
     con.commit()
+
+
+def insert_data(con, df):
+    cursor = con.cursor()
+    for index, row in df.iterrows():
+        cursor.execute('''
+        INSERT INTO sales (product, quantity, price)
+        VALUES (?, ?, ?)''',(
+
+
+               row['product'], 
+               row['quantity'],
+               row['price']
+
+        ))
+    
+    
+    con.commit()  
