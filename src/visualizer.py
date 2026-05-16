b50816a09e708c6808b20f99fc34c77d6befc399
Nistha <nisthabhattacharjee7@gmail.com>
2026-05-16 10:44:45 +0530

    added category revenue chart

--- src/visualizer.py
+++ src/visualizer.py
@@ -23,3 +23,28 @@ def revenue_chart (df):
     plt.savefig('output/revenue_chart.png')
     print("\nRevenue chart generated successfully")
 
+
+def category_chart(df):
+
+    # create a revenue column 
+    df['revenue'] = df['quantity'] * df['price']
+
+    # category names 
+    categories = df['category'].unique()
+
+    # Group revenue by category 
+    category_revenue = df.groupby('category')['revenue'].sum()
+
+    # create chart 
+    plt.bar(categories, category_revenue)
+
+    # chart title 
+    plt.title('Revenue by Category')
+
+    # axis labels
+    plt.xlabel('Category')
+    plt.ylabel('Revenue')
+
+    # save chart
+    plt.savefig('output/category_chart.png')
+    print("\nCategory revenue chart generated successfully")
