d9ba18a897cc735d632984b73eaea2bc76d07d74
Nistha <nisthabhattacharjee7@gmail.com>
2026-05-17 12:05:49 +0530

    fixed error and added category revenue

--- src/database.py
+++ src/database.py
@@ -7,10 +7,10 @@ def create_connection():
 def create_table(con):
     cursor = con.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXISTS sales
-                      (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                       product TEXT NOT NULL,
-                       quantity INTEGER NOT NULL,
-                       price REAL NOT NULL)''')
+                       ( product TEXT,
+                        quantity REAL,
+                        price REAL,
+                        category TEXT)''')
     con.commit()
 
 
@@ -18,14 +18,14 @@ def insert_data(con, df):
     cursor = con.cursor()
     for index, row in df.iterrows():
         cursor.execute('''
-        INSERT INTO sales (product, quantity, price)
-        VALUES (?, ?, ?)''',(
+        INSERT INTO sales (product, quantity, price, category)
+        VALUES (?, ?, ?, ?)''',(
 
 
                row['product'], 
                row['quantity'],
-               row['price']
-
+               row['price'],
+               row['category']
         ))
     
     
@@ -39,4 +39,43 @@ def get_total_revenue(con):
     FROM sales''')
     
     total_revenue = cursor.fetchone()
-    return [0]
+    return total_revenue[0]
+
+
+def get_category_revenue(con):
+    cursor = con.cursor()
+    cursor.execute('''
+    SELECT category, SUM(quantity * price) 
+    FROM sales
+    GROUP BY category''')
+    
+    category_revenue = cursor.fetchall()
+    return category_revenue
+
+
+def  get_top_products(con):
+    cursor = con.cursor()
+    cursor.execute('''
+    SELECT product, 
+    SUM(quantity) as total_quantity
+    FROM sales
+    GROUP BY product
+    ORDER BY total_quantity DESC
+    ''')
+    
+    top_products = cursor.fetchall()
+    return top_products
+
+
+def get_category_counts(con):
+    cursor = con.cursor()
+    cursor.execute('''
+    SELECT category,
+    COUNT(*) AS total_products
+    FROM sales
+    GROUP BY category''')
+    
+    category_counts = cursor.fetchall()
+    return category_counts
+
+
