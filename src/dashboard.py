77b619c2d81a8562161764cac636f5a2a871afa3
Nistha <nisthabhattacharjee7@gmail.com>
2026-05-17 14:33:37 +0530

    added bar chart

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,5 +1,6 @@
 import streamlit as st 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from cleaner import clean_data
 from analyzer import analyze_df
@@ -29,7 +30,21 @@ st.metric("Top Product", top_products)
 st.header("Cleaned Sales Data")
 st.dataframe(cleaned_df)
 
+# Revenue by product chart 
+st.header("Revenue by Product")
 
+cleaned_df['revenue'] =(
+    cleaned_df['quantity'] *
+    cleaned_df['price']
+)
+
+product_revenue = cleaned_df.groupby('product')['revenue'].sum()
+fig, ax = plt.subplots()
+product_revenue.plot(
+    kind='bar', 
+    ax=ax
+)
+st.pyplot(fig)
 
 
 
